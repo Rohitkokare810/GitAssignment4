623ba47a0a007686cca6b48068b2c59977255039
RohitKokare810 <Rohitkokare810@gmail.com>
2023-11-10 15:21:55 +0530

    files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Task 1: Declaration and Initialization
+        int[][] matrix = new int[][]
+        {
+            new int[] {1, 2, 3, 4},
+            new int[] {5, 6},
+            new int[] {7, 8, 15}
+        };
+
+        // Task 2: Accessing and Display
+        int element = matrix[0][1];
+
+        Console.WriteLine("Task 2: Displaying Matrix");
+        DisplayMatrix(matrix);
+
+        // Task 3: Modification
+        matrix[1][2] = 15;
+
+        Console.WriteLine("\nTask 3: Modified Matrix");
+        DisplayMatrix(matrix);
+
+        // Task 4: Advanced Concepts
+        object[][] objectMatrix = new object[][]
+        {
+            new object[] {10, "Hello", 3.14},
+            new object[] {true, 'A', DateTime.Now}
+        };
+
+        Console.WriteLine("\nTask 4: Displaying Object Matrix");
+        DisplayObjectMatrix(objectMatrix);
+    }
+
+    // Helper method to display the jagged array of integers
+    static void DisplayMatrix(int[][] matrix)
+    {
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            for (int j = 0; j < matrix[i].Length; j++)
+            {
+                Console.Write(matrix[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    // Helper method to display the jagged array of objects
+    static void DisplayObjectMatrix(object[][] objectMatrix)
+    {
+        for (int i = 0; i < objectMatrix.Length; i++)
+        {
+            for (int j = 0; j < objectMatrix[i].Length; j++)
+            {
+                Console.Write($"{objectMatrix[i][j]} ({objectMatrix[i][j].GetType()}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        // Task 1: ArrayList Initialization and Population
+        ArrayList studentGrades = new ArrayList() { 85, 92, 78, 95, 88 };
+
+        // Task 2: Display and Sum
+        Console.WriteLine("Task 2: Displaying studentGrades and Calculating Sum");
+        DisplayStudentGrades(studentGrades);
+        DisplaySumOfGrades(studentGrades);
+
+        // Task 3: Grade Addition
+        studentGrades.Add(90);
+
+        Console.WriteLine("\nTask 3: Adding a new grade (90)");
+        DisplayStudentGrades(studentGrades);
+
+        // Task 4: Grade Removal
+        studentGrades.Remove(78);
+
+        Console.WriteLine("\nTask 4: Removing the grade 78");
+        DisplayStudentGrades(studentGrades);
+
+        // Task 5: Search and Update
+        int indexOfGrade95 = studentGrades.IndexOf(95);
+
+        if (indexOfGrade95 != -1)
+        {
+            studentGrades[indexOfGrade95] = 96;
+            Console.WriteLine("\nTask 5: Updating the grade at index " + indexOfGrade95 + " to 96");
+            DisplayStudentGrades(studentGrades);
+        }
+        else
+        {
+            Console.WriteLine("\nTask 5: Grade 95 not found in studentGrades.");
+        }
+    }
+
+    // Helper method to display the contents of the ArrayList
+    static void DisplayStudentGrades(ArrayList studentGrades)
+    {
+        foreach (var grade in studentGrades)
+        {
+            Console.WriteLine(grade);
+        }
+    }
+
+    // Helper method to calculate and display the sum of grades in the ArrayList
+    static void DisplaySumOfGrades(ArrayList studentGrades)
+    {
+        int sum = 0;
+        foreach (var grade in studentGrades)
+        {
+            sum += (int)grade;
+        }
+        Console.WriteLine("\nSum of all grades: " + sum);
+        Console.ReadKey();
+    }
+    
+}
